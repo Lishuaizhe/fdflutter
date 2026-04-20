1a89ea78657a420fdde95e9be46b20de1dd684aa
Zach Li <litongxue1999@gmail.com>
2026-04-20 16:24:20 +0800

    chore: update lockfile and clean up naming

--- lib/core/localization/config.dart
+++ lib/core/localization/config.dart
@@ -7,21 +7,21 @@ mixin AppLocale {
   static const String competed = 'competed';
   static const String notCompleted = 'notCompleted';
 
-  static const Map<String, dynamic> EN = {
+  static const Map<String, dynamic> en = {
     todoPageTitle: 'Todo List',
     settingPageTitle: 'settings',
     thisIs: 'This is %a package, version %a.',
     competed: 'Completed',
     notCompleted: 'Not Completed',
   };
-  static const Map<String, dynamic> ZH = {
+  static const Map<String, dynamic> zh = {
     todoPageTitle: '待办列表',
     settingPageTitle: '设置',
     thisIs: '这是%a的包，版本 %a。',
     competed: '已完成',
     notCompleted: '未完成',
   };
-  static const Map<String, dynamic> JA = {
+  static const Map<String, dynamic> ja = {
     todoPageTitle: 'Todoリスト',
     settingPageTitle: '設定',
     thisIs: 'これは%aパッケージ、バージョン%aです。',
@@ -33,19 +33,19 @@ mixin AppLocale {
 const mapLocales = [
   MapLocale(
     'en',
-    AppLocale.EN,
+    AppLocale.en,
     countryCode: 'US',
     fontFamily: 'Font EN',
   ),
   MapLocale(
     'zh',
-    AppLocale.ZH,
+    AppLocale.zh,
     countryCode: 'ZH',
     fontFamily: 'Font ZH-CN',
   ),
   MapLocale(
     'ja',
-    AppLocale.JA,
+    AppLocale.ja,
     countryCode: 'JP',
     fontFamily: 'Font JA',
   ),
